28dbda15861f8b2a5c46881ce56a7401e0d9e238
aerem2910 <138059430+aerem2910@users.noreply.github.com>
2024-03-21 00:41:18 +0300

    Update ServiceChat.cs

--- DiplomaProject/ServiceChat.cs
+++ DiplomaProject/ServiceChat.cs
@@ -35,49 +35,7 @@ namespace DiplomaProject
                 SendMessage($"{user.Name} покинул беседу ", 0);
             }
         }
-        /*
-        public void SendImage(ImageMessage imageMessage, int id)
-        {
-            foreach (var item in users.ToList())
-            {
-                try
-                {
-            //        var callback = item.OperationContext.GetCallbackChannel<IServerChatCallback>();
-           //        callback.ImageCallback(imageMessage, id, item.Name);
-             / /  }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Ошибка отправки изображения пользователю {item.Name}: {ex.Message}");
-                }
-            }
-        }
-        */
-
-        /*public void SendImage(byte[] imageData, string imageName, int id)
-         {
-
-             foreach (var item in users.ToList())
-             {
-                 try
-                 {
-                     var callback = item.OperationContext.GetCallbackChannel<IServerChatCallback>();
-                     var imageMessage = new ImageMessage
-                     {
-                         ImageName = imageName,
-                         ImageData = imageData
-                     };
-                    // callback.ImageCallback(imageMessage, id, item.Name);
-                 }
-                 catch (Exception ex)
-                 {
-                     Console.WriteLine($"Ошибка отправки изображения пользователю {item.Name}: {ex.Message}");
-                 }
-             }
-
-             // Добавьте вызов метода для отображения изображения в интерфейсе
-            // DisplayImage(imageData, imageName);
-         }*/
-
+       
         public void SendImage(byte[] imageData, string imageName, int id)
         {
             foreach (var item in users.ToList())
@@ -122,30 +80,7 @@ namespace DiplomaProject
                 }
             }
         }
-        /*public void SendMessage(string message, int id)
-        {
-            foreach (var item in users.ToList())
-            {
-                string answer = $"{DateTime.Now.ToShortTimeString()}";
-                var user = users.FirstOrDefault(i => i.ID == id);
-                if (user != null)
-                {
-                    answer += $": {user.Name} ";
-                }
-                answer += message;
-
-                try
-                {
-                    var callback = item.OperationContext.GetCallbackChannel<IServerChatCallback>();
-                    callback.MessageCallback(answer);
-                }
-                catch (Exception ex)
-                {
-                    Console.WriteLine($"Error sending message to user {item.Name}: {ex.Message}");
-                }
-            }
-        }
-        */
+        
         public void ImageCallback(ImageMessage imageMessage, int id, string senderName)
         {
             foreach (var item in users.ToList())
